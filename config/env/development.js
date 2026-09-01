a8ec28dc7ea0f6bf91cd073c00636168dce647f4
nsushma-bd <nsushma@blackduck.com>
2026-09-01 13:16:30 +0530

    update file config/env/development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
    // If you want to debug regression tests, you will need the following which is also in the test config:
-   zapHostName: "192.168.56.20",
+   zapHostName: process.env.ZAP_HOST_NAME || "localhost",
    zapPort: "8080",
    // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
    zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
